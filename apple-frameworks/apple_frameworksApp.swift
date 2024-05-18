17941b26cb6febf20e0ae99caa53e714124fcd47
João Pedro Leopoldino <leopoldinodev@gmail.com>
2024-05-18 15:05:58 -0300

    Initial Commit

---
+++ apple-frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple-frameworks
+//
+//  Created by João Pedro Leopoldino on 2024-05-18.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
