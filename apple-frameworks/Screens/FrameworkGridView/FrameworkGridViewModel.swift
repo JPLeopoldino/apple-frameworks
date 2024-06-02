e02ffa53b47b7191a5626a67a4ec2e37b2f81c94
João Pedro Leopoldino <leopoldinodev@gmail.com>
2024-06-02 16:27:24 -0300

    feat: ensure initial project structure

---
+++ apple-frameworks/Screens/FrameworkGridView/FrameworkGridViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FrameworkGridViewModel.swift
+//  apple-frameworks
+//
+//  Created by João Pedro Leopoldino on 2024-05-18.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet { isShowingDetailView = true }
+    }
+    
+    @Published var isShowingDetailView = false
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+}
---
+++ apple-frameworks/UIKit Components/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  apple-frameworks
+//
+//  Created by João Pedro Leopoldino on 2024-06-02.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
